93a58c5b3b0769f15ca1d6766e3aae05418591b9
Ignatius S. Condro A.B <saptocondro@gmail.com>
2022-08-09 10:34:42 +0200

    add CUDA files to test if CUDA can run.
    The codes are working.

---
+++ Course2022_Udemy_CUDA_Programming_Masterclass_Cpp/Section01_Intro/t05_Basic_elements_CUDA/CudaTestRun01/cuda_kernel.cuh
@@ -0,0 +1,18 @@
+/**
+* CUDA Test Run cuh file
+*
+* it is working, after compiled with
+* - Visual C++ 2022
+* - CUDA 11.7
+* - a main function in a cpp file, including a cuh header file
+*
+* version: 08:20 09.08.2022
+*/
+
+void kernel(double* A, double* B, double* C, int arraySize);
+
+/**
+* version: 08:20 09.08.2022
+*
+* End of file
+*/
