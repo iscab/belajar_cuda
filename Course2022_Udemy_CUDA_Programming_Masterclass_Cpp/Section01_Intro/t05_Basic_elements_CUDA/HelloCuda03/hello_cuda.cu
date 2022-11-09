cf92ef8f139b15efd6e061872f2658eacd6ceb8e
Ignatius S. Condro A.B <saptocondro@gmail.com>
2022-11-09 21:27:27 +0100

    add CUDA files about Hello World: now it is working but I don't really know why

---
+++ Course2022_Udemy_CUDA_Programming_Masterclass_Cpp/Section01_Intro/t05_Basic_elements_CUDA/HelloCuda03/hello_cuda.cu
@@ -0,0 +1,37 @@
+﻿/**
+* Hello CUDA World
+*
+* it is not working, after compiled with
+* - Visual C++ 2022
+* - CUDA 11.7
+* no compiling error, but I don't know why  
+* notes:  use CUDA option in New Project in Visual C++ 2022  
+* 
+* 
+* version: 10:45 09.11.2022
+*/
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+__global__ void hello_cuda()
+{
+	printf("Hello CUDA World \n");
+}
+
+int main()
+{
+	hello_cuda << < 1, 1 >> > ();
+	cudaDeviceSynchronize();
+
+	cudaDeviceReset();
+	return 0;
+}
+
+/**
+* version: 10:45 09.11.2022
+*
+* End of file
+*/
